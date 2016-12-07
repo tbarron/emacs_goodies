8dae84cec27912654471cd20adfed974383a7ef0
Tom Barron <tbarron@ornl.gov>
2016-12-07 15:10:22 -0500

    - Add command dtw-and-save, which calls delete-trailing-whitespace,    then saves the buffer

--- mykeys.el
+++ mykeys.el
@@ -3,3 +3,10 @@
 ;;;; ===============================================================
 (global-set-key "\M-\\" 'indent-region)
 (global-set-key "\C-y" 'clipboard-yank)
+(defun dtw-and-save ()
+  "Delete trailing whitespace then save the file"
+  (interactive)
+  (delete-trailing-whitespace)
+  (save-buffer)
+)
+(global-set-key "\C-@" 'dtw-and-save)
