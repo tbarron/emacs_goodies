2c8ae19c36303dd4b2d475dfe9cbead193ae50cc
Tom Barron <tusculum@gmail.com>
2019-12-06 06:11:33 -0500

    Tweaks and notes in mykeys.el

--- mykeys.el
+++ mykeys.el
@@ -1,9 +1,13 @@
 ;;;; ===============================================================
 ;;;; Key bindings
 ;;;; ===============================================================
+(global-set-key "\C-c\C-f" 'forward-sexp)
+(global-set-key "\C-c\C-b" 'backward-sexp)
+(global-set-key "\C-x\C-c" 'save-buffers-kill-terminal)
 (global-set-key "\M-b" 'eval-buffer)
 (global-set-key "\M-\\" 'indent-region)
 (global-set-key "\C-y" 'clipboard-yank)
+(global-set-key "\C-c\C-a" 'indent-pp-sexp)
 
 (defun clean-buffer ()
   (interactive)
@@ -17,3 +21,14 @@
   (insert (shell-command-to-string "pbpaste"))
 )
 (global-set-key "\M-v" 'osx-paste)
+
+;; available key sequences
+;; \C-c a
+;; \C-c b
+;; \C-c c
+;; \C-c d
+;; \C-c e
+;; \C-c f
+;; \C-c g
+;; \C-c h
+
