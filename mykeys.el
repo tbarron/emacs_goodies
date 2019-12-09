6387e13cd69dbe1fdcceb5367c904e681ed4d784
Tom Barron <tusculum@gmail.com>
2019-12-09 05:53:58 -0500

    Commands copy-comment-{line,region}

--- mykeys.el
+++ mykeys.el
@@ -22,6 +22,37 @@
 )
 (global-set-key "\M-v" 'osx-paste)
 
+(defun copy-comment-line ()
+  "Copy the current line and comment the copy"
+  (interactive)
+  (save-excursion
+    (let ((start) (end) (text))
+      (beginning-of-line)
+      (setq start (point))
+      (forward-line 1)
+      (setq end (point))
+      (copy-region-as-kill start end)
+      (yank)
+      (setq end (point))
+      (forward-line -1)
+      (comment-region (point) end)
+      )))
+(global-set-key "\C-cc" 'copy-comment-line)
+
+(defun copy-comment-region (start end)
+  "Copy the marked region, insert it after a blank line, then
+comment the copy."
+  (interactive "*r")
+  (save-excursion
+    (if (not mark-active)
+        (message "Please set the mark (\e-space) and try again.")
+      (goto-char end)
+      (insert "\n" (buffer-substring start end) "\n")
+      (comment-region end (point)))
+    ))
+(global-set-key "\C-c\C-y" 'copy-comment-region)
+
+(message "buffer mykeys.el loaded")
 ;; available key sequences
 ;; \C-c a
 ;; \C-c b
